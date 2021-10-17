54538251af9f125d723ae26057526a007655fc03
prafulpatel1999 <88098316+prafulpatel1999@users.noreply.github.com>
2021-10-17 14:54:31 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,21 @@
+const answer="90"
+let total =prompt("solve this buddy (4+5)x(5+5)")
+
+while( answer !== total){
+    total=prompt("solve again")
+}
+
+
+const answer1="0.25"
+let total1 =prompt("solve this too buddy (2+3)+(3+2)/(5x8)")
+
+while( answer1 !== total1){
+    total1=prompt("solve again")
+}
+const answer2="3"
+let total2 =prompt("solve this too buddy (10x3)/(5+5)")
+
+while( answer2 !== total2){
+    total2=prompt("solve again")
+}
+alert("game over well played")
